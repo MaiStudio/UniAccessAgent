65e848bd13d109958d44a840c35e93e5f0c839f5
Microsoft <foxhut@outlook.com>
2021-02-18 01:39:35 +0800

    Bypass the agent monitoring to access the internet

---
+++ MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniAccessAgent
+{
+    public partial class frmSafeAssistant : Form
+    {
+        public frmSafeAssistant()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSafeAssistant_Load(object sender, EventArgs e)
+        {
+            this.Visible = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace UniAccessAgent
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Utilities.RefreshTrayArea();//Clear last time notify icon
+
+            bool appRunning = false;
+            Mutex mutex = new Mutex(true, Process.GetCurrentProcess().ProcessName, out appRunning);
+
+            if (!appRunning)
+                Environment.Exit(0);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmSafeAssistant());
+            
+            mutex.ReleaseMutex();
+        }
+    }
+}
